085bea6e96bc12c7a672d612f87892bc9c2a622c
Oksana Semenkova <ext-oksana.semenkova@here.com>
2021-10-18 19:52:08 +0300

    GroupWordsTask was implemented

--- Basics/Monads & Collections/Group Words/src/GroupWordsTask.scala
+++ Basics/Monads & Collections/Group Words/src/GroupWordsTask.scala
@@ -1,3 +1,8 @@
 object GroupWordsTask {
-  def groupIt(wordsSeq: Seq[String]): Map[Char, Int] = ???
+  def groupIt(wordsSeq: Seq[String]): Map[Char, Int] = {
+    wordsSeq.groupBy(_.charAt(0)).transform((key, value) => countOccurences(value, key))
+  }
+  def countOccurences(words: Seq[String], ch: Char): Int = {
+    words.map(c => c.count(x => {x == ch})).sum
+  }
 }
