26e036c14759881b5b95b8bb58c7254dcccbe9c7
Oksana Semenkova <ext-oksana.semenkova@here.com>
2021-10-20 01:42:56 +0300

    ZipWordTask was implemented

--- Basics/Monads & Collections/Zip A Word/src/ZipWordTask.scala
+++ Basics/Monads & Collections/Zip A Word/src/ZipWordTask.scala
@@ -1,3 +1,9 @@
 object ZipWordTask {
-  def zipIt(prefixSeq: Seq[String], wordSeq: Seq[String], suffixSeq: Seq[String]): Seq[String] = ???
+  def zipIt(prefixSeq: Seq[String], wordSeq: Seq[String], suffixSeq: Seq[String]): Seq[String] = {
+//    prefixSeq.zip(suffixSeq).map{case(x,y) => x+y}
+    def zip[X,Y,Z](xs: Seq[X], ys: Seq[Y], zs: Seq[Z]): Seq[(X,Y,Z)] = {
+      for (((x, y), z) <- xs zip ys zip zs) yield (x, y, z)
+    }
+    zip(prefixSeq, wordSeq, suffixSeq).map{case(x,y,z) => x+y+z}
+  }
 }
